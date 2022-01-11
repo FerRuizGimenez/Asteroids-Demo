9ea7afcbc46490112ab11cb4716f46745815da3c
Fernando Ruiz Gimenez <fer.ruizgimenez@gmail.com>
2022-01-11 13:58:30 -0300

    Explosion Particle Effect

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -49,6 +49,7 @@ public class Asteroid : MonoBehaviour
                 CreateSplit();
             }
 
+            FindObjectOfType<GameManager>().AsteroidDestroyed(this);
             Destroy(this.gameObject);
         }
     }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,12 +3,22 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public Player player;
+    public ParticleSystem explosion;
     public float respawnTime = 3.0f;
     public float respawnInvulnerabilityTime = 3.0f;
     public int lives = 3;
+    public int score = 0;
+
+    public void AsteroidDestroyed(Asteroid asteroid)
+    {
+        this.explosion.transform.position = asteroid.transform.position;
+        this.explosion.Play();
+    }
 
     public void PlayerDied()
     {
+        this.explosion.transform.position = this.player.transform.position;
+        this.explosion.Play();
         this.lives--;
 
         if(this.lives <= 0)
